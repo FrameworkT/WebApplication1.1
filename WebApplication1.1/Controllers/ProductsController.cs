f73a83f4c4094d84e09921fc6f7110034709aedc
Pradipat - <Pradipattae@gmail.com>
2024-10-26 15:17:19 +0700

    Add project files.

---
+++ WebApplication1.1/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1._1.Services;
+
+namespace WebApplication1._1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService ps;
+
+        public ProductsController(IProductService ps)
+        {
+            this.ps = ps;
+        }
+        public IActionResult Index()
+        {
+            return View(ps.GetProducts());
+        }
+        public IActionResult Delete(int id) 
+        {
+
+            ps.DeleteData(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
